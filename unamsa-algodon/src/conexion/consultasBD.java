de58c5ed44ed1273916950e7b09f0b7cfe9b047d
bruce <bruce@MSI>
2022-06-27 22:48:49 -0600

    Se agrego el metodo de captura Cliente en la clase consultasBD

--- unamsa-algodon/src/conexion/consultasBD.java
+++ unamsa-algodon/src/conexion/consultasBD.java
@@ -26,35 +26,42 @@ public class consultasBD {
         
     }
     
-    public void registrarCliente(String nombre, String apellidos, String telefono, String direccion){
+       
+    public void registrarCliente(String nombre, String direccion, int codigoPostal, String RFC, int idRegimenFiscal, int idCuentaBancaria, String telefono, String correo, int idContador){
         PreparedStatement ps;
         String sql;
         cliente.setNombre(nombre);
-        cliente.setApellidos(apellidos);
-        cliente.setTelefono(telefono);
         cliente.setDireccion(direccion);
+        cliente.setCodigoPostal(codigoPostal);
+        cliente.setRFC(RFC);
+        cliente.setIdRegimenFiscal(idRegimenFiscal);
+        cliente.setIdCuentaBancaria(idCuentaBancaria);
+        cliente.setTelefono(telefono);
+        cliente.setCorreo(correo);
+        cliente.setIdContador(idContador);
         
-        
+     
         
         try{
             con = conectar.getConexion();
-            sql = "INSERT INTO clientes(nombre, apellidos, telefono, direccion) values(?,?,?,?)";
+            sql = "INSERT INTO clientes(nombre, direccion, codigoPostal, RFC, fkRegimen, fkCuentaBancaria, telefono, correo, fkContador) values(?,?,?,?,?,?,?,?,?)";
             
             ps = con.prepareStatement(sql);
             
             ps.setString(1, cliente.getNombre());
-            ps.setString(2, cliente.getApellidos());
-            ps.setString(3, cliente.getTelefono());
-            ps.setString(4, cliente.getDireccion());
+            ps.setString(2, cliente.getDireccion());
+            ps.setInt(3, cliente.getCodigoPostal());
+            ps.setString(4, cliente.getRFC());
+            ps.setInt(5, cliente.getIdRegimenFiscal());
+            ps.setInt(6, cliente.getIdCuentaBancaria());
+            ps.setString(7, cliente.getTelefono());
+            ps.setString(8, cliente.getCorreo());
+            ps.setInt(9, cliente.getIdContador());
             
             ps.execute();
             JOptionPane.showMessageDialog(null, "Se han insertado los datos");
         }catch(SQLException e){
             JOptionPane.showMessageDialog(null, "Error de conexión:" + e.getMessage());
         }
-    }
-
-    public consultasBD(clientes cliente) {
-        this.cliente = cliente;
     }
 }
